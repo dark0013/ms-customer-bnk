66f93dcdeb279e6dd5cf3f215ef79dc229216a23
DARKROSS <andresarcangel00@gmail.com>
2025-07-30 03:16:46 -0500

    add status http

--- src/main/java/com/example/ms_customer_bnk/infraestructure/in/controller/CustomerController.java
+++ src/main/java/com/example/ms_customer_bnk/infraestructure/in/controller/CustomerController.java
@@ -2,8 +2,15 @@ package com.example.ms_customer_bnk.infraestructure.in.controller;
 
 import com.example.ms_customer_bnk.infraestructure.in.dto.CustomerDto;
 import com.example.ms_customer_bnk.infraestructure.in.handler.CustomerHandler;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,27 +24,30 @@ public class CustomerController {
     }
 
     @GetMapping
-    public List<CustomerDto> finAllCustomer(){
-        return customerHandler.findAllCustomers();
+    public ResponseEntity<List<CustomerDto>> finAllCustomer() {
+        return new ResponseEntity<>(customerHandler.findAllCustomers(), OK);
     }
 
     @GetMapping("/{id}")
-    public CustomerDto findCustomerById(@PathVariable Integer id){
-    return customerHandler.findCustomerById(id);
+    public ResponseEntity<CustomerDto> findCustomerById(@PathVariable Integer id) {
+        return new ResponseEntity<>(customerHandler.findCustomerById(id), OK);
     }
 
     @PostMapping
-    public CustomerDto saveCustomer(@RequestBody CustomerDto customer){
-        return customerHandler.saveCustomer(customer);
+    public ResponseEntity<CustomerDto> saveCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto customer = customerHandler.saveCustomer(customerDto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(customer.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping
-    public CustomerDto updateCustomer(@RequestBody CustomerDto customer){
-        return customerHandler.updateCustomer(customer);
+    public ResponseEntity<CustomerDto> updateCustomer(@RequestBody CustomerDto customer) {
+        return new ResponseEntity<>( customerHandler.updateCustomer(customer),OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCustomer(@PathVariable Integer id){
-         customerHandler.deleteCustomer(id);
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Integer id) {
+        customerHandler.deleteCustomer(id);
+        return new ResponseEntity<>(NO_CONTENT);
     }
 }
