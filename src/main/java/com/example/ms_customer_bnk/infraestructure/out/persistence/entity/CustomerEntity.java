1427f749efeefddace2944dd48b0d71b2d66106b
DARKROSS <andresarcangel00@gmail.com>
2025-07-31 06:51:25 -0500

    implement lombok

--- src/main/java/com/example/ms_customer_bnk/infraestructure/out/persistence/entity/CustomerEntity.java
+++ src/main/java/com/example/ms_customer_bnk/infraestructure/out/persistence/entity/CustomerEntity.java
@@ -1,9 +1,13 @@
 package com.example.ms_customer_bnk.infraestructure.out.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tbl_adm_customer")
+@Getter
+@Setter
 public class CustomerEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,59 +19,4 @@ public class CustomerEntity {
     private String address;
     private String telephone;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getIdentification() {
-        return identification;
-    }
-
-    public void setIdentification(String identification) {
-        this.identification = identification;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
